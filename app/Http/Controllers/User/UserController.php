4d134cc74dd58f9b207c5c0a33c40a1e8078f40c
ihor25-admin <ihor.p@devpark.pl>
2022-10-02 22:52:40 +0200

    INZ-0 show users endpoint without tests

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,19 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\User;
 
-use App\Models\User;
 use UseCases\User\UserCase;
-use UseCases\User\UserDrivers;
-use App\Models\ResponseMessages;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\User\UserResource;
 use App\Http\Requests\User\UsersListRequest;
-use App\Http\Requests\User\UpdateUserRequest;
 use Symfony\Component\HttpFoundation\Response;
-use UseCases\Contracts\ResponseObjects\IError;
-use App\Http\Requests\User\ActivateUserRequest;
 use App\Http\Resources\User\UsersCollectionResource;
-use App\Http\Resources\User\DriversCollectionResource;
 
 /**
  * @see UserControllerOA
